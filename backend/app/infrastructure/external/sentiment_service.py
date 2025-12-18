393941536c5ccfaa9a1e39736d11827f3c93b8a6
Eng-Laith Alskaf <84410550+laith-alskaf@users.noreply.github.com>
2025-12-18 23:55:10 +0300

    Update cabackendndidate_labels for detect_context_mismatch

--- backend/app/infrastructure/external/sentiment_service.py
+++ backend/app/infrastructure/external/sentiment_service.py
@@ -259,7 +259,7 @@ class SentimentService:
 
                             target_score = result_map.get(target_label, 0.0)+result_map.get(candidate_labels[1], 0.0)
 
-                            if top_score < 0.45:
+                            if top_score < 0.5:
                                 has_mismatch = True
                                 predicted_label = "غير مرتبط"
                             else:
